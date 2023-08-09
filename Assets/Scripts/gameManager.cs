e9f2232c4bd530021217d5b6560616270d040e86
JangbeomChoi <jangbeom0325@gmail.com>
2023-08-09 11:57:58 +0900

    카드 위치 중앙으로 옮김

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -17,8 +17,8 @@ public class gameManager : MonoBehaviour
             GameObject newCard = Instantiate(card);
             newCard.transform.parent = GameObject.Find("cards").transform;
 
-            float x = (i / 4) * 1.4f;
-            float y = (i % 4) * 1.4f;
+            float x = (i / 4) * 1.4f - 2.1f;
+            float y = (i % 4) * 1.4f - 3.0f;
             newCard.transform.position = new Vector3(x, y, 0);
         }
     }
